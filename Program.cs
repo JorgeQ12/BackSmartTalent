358eca60dc4abeccca2a06f1e8ac52a530a09ae7
Jorge Quintero <quinterojorge1234@gmail.com>
2023-12-11 13:59:09 -0500

    Desplegar Azure

--- Program.cs
+++ Program.cs
@@ -41,12 +41,8 @@ builder.Services.AddTransient<SendEmail>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
